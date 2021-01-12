827738aaadf6bac5cf582b05c43f79a0bab3c4d8
Abhishek Gangwar <me.abhishekgangwar@gmail.com>
2021-01-12 23:34:26 +0530

    feat: adding async await syntax

--- src/index.js
+++ src/index.js
@@ -30,6 +30,33 @@ app.post('/users',(req,res) =>{
     }
 })
 
+app.get('/users', async (req,res) =>{
+    try{
+        const users = await User.find({});
+        if(!users){
+           return res.status(404).send();
+        }
+        res.send(users);
+    }catch(e){
+        console.log(chalk.red(e))
+    }
+})
+
+
+app.get('/users/:id', async (req,res) =>{
+    try{
+        const {id} = req.params;
+        const user = await User.findById(id);
+        if(!user){
+            console.log(chalk.red('Unable to find User by given Id'))
+           return res.status(404).send('Unable to find User by given Id');
+        }
+        res.send(user);
+    }catch(e){
+        console.log(chalk.red(e))
+    }
+})
+
 app.post('/tasks',(req, res) =>{
      if(req){
         const task = new Task(req.body)
@@ -40,6 +67,31 @@ app.post('/tasks',(req, res) =>{
             res.status(400).send(e.message)
             console.log(chalk.red(e))
         })
+    }
+})
+
+app.get('/tasks', async (req,res) =>{
+    try{
+        const tasks = await Task.find({});
+        if(!tasks){
+           return res.status(404).send();
+        }
+        res.send(tasks);
+    }catch(e){
+        console.log(chalk.red(e))
+    }
+})
+
+app.get('/tasks/:id', async (req,res) =>{
+    try{
+        const {id} = req.params;
+        const task = await Task.findById(id);
+        if(!task){
+           return res.status(404).send();
+        }
+        res.send(task);
+    }catch(e){
+        console.log(chalk.red(e))
     }
 })
 
